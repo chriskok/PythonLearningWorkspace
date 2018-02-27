bb81366cac528e4f902b4920981d7a920e56fc08
chriskok <ckok@purdue.edu>
2018-02-27 08:21:54 -0500

    Finished Tutorial 3
    - Updated unicode translator challenge to include lower case letters

--- unicodetranslator.py
+++ unicodetranslator.py
@@ -7,15 +7,19 @@
 
 userInput = input("Enter a string to hide in uppercase: ")
 
+# Cycle each character in the string and change to unicode
 newString = ""
 for char in userInput:
-    newString += str(ord(char))
+    newString += str(ord(char) - 23)
 
+# Print the secret message
 print("Secret Message:", newString)
 
+# Cycle through each char 2 at a time and change the digits to characters
 newOutput = ""
 for i in range(0, len(newString)-1, 2):
-    newOutput += chr(int(newString[i] + newString[i+1]))
-    #print("int to convert:", int(newString[i] + newString[i+1]))
+    newOutput += chr(int(newString[i] + newString[i+1]) + 23)
+    # print("int to convert:", int(newString[i] + newString[i+1]))
 
+# Print the original message
 print("Original Message:", newOutput)
