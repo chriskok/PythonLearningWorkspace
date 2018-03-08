c13465016345fa8a2def9571fe8e6667fe763267
Christopher Kok <ckok@purdue.edu>
2018-03-08 08:42:54 -0500

    Tutorial10 - Polymorphism

--- tutorial10-inheritance.py
+++ tutorial10-inheritance.py
@@ -1,13 +1,13 @@
 class Animal:
     def __init__(self, birthType="Unknown", appearance="Unknown",
                  blooded="Unknown"):
-        self.birth = birthType
+        self.birthType = birthType
         self.appearance = appearance
         self.blooded = blooded
 
     @property
     def birthType(self):
-        return self.birthType
+        return self.__birthType
 
     @birthType.setter
     def birthType(self, birthType):
@@ -15,7 +15,7 @@ class Animal:
 
     @property
     def appearance(self):
-        return self.appearance
+        return self.__appearance
 
     @appearance.setter
     def appearance(self, appearance):
@@ -23,7 +23,7 @@ class Animal:
 
     @property
     def blooded(self):
-        return self.blooded
+        return self.__blooded
 
     @blooded.setter
     def blooded(self, blooded):
@@ -43,18 +43,52 @@ class Mammal(Animal):
 
     @property
     def nurseYoung(self):
-        return self.nurseYoung
+        return self.__nurseYoung
 
     @nurseYoung.setter
     def nurseYoung(self, nurseYoung):
         self.__nurseYoung = nurseYoung
 
+    # MAGIC METHOD
     def __str__(self):
-        return super().__str__() + " and it is {} they nurse"\
-                "their young".format(self.nurseYoung)
+        return super().__str__() + " and it is {} they nurse" \
+                                   "their young".format(self.nurseYoung)
 
 
-class Reptile(Animal)
+class Reptile(Animal):
     def __init__(self, birthType="born in egg or alive",
                  appearance="dry scales", blooded="cold blooded"):
         Animal.__init__(self, birthType, appearance, blooded)
+
+    def sumAll(self, *args):
+        sum = 0
+        for i in args:
+            sum += i
+        return sum
+
+
+def getBirthType(theObject):
+    print("The {} is {}".format(type(theObject).__name__,
+                                theObject.birthType))
+
+
+def main():
+    animal1 = Animal("born alive")
+    print(animal1.__str__())
+    print(animal1)  # same thing as above
+
+    mammal1 = Mammal()
+    print(mammal1.__str__())
+
+    reptile1 = Reptile()
+    print(reptile1.birthType)
+    print(reptile1.appearance)
+    print(reptile1.blooded)
+    print(reptile1)
+    print("Sum: {}".format(reptile1.sumAll(1, 2, 3, 4, 5)))
+
+    getBirthType(mammal1)
+    getBirthType(reptile1)
+
+
+main()
