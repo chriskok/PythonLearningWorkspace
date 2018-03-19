612ef2d94894a16ba95f146f235901c3e18066df
Christopher Kok <ckok@purdue.edu>
2018-03-19 10:04:01 -0400

    Tutorial12 - Advanced Function Challenge

--- tutorial12-advfunctions.py
+++ tutorial12-advfunctions.py
@@ -42,33 +42,6 @@ listOfFuncs = [times_two, generated_func]
 
 print("5 * 9 =", listOfFuncs[1](9))
 
-# ---------- PROBLEM ----------
-# Create a function that receives a list and a function
-# The function passed will return True or False if a list
-# value is odd.
-# The surrounding function will return a list of odd
-# numbers
-
-def is_it_odd(num):
-    if num % 2 == 0:
-        return False
-    else:
-        return True
-
-def change_list(list, func):
-
-    oddList = []
-
-    for i in list:
-        if func(i):
-            oddList.append(i)
-
-    return oddList
-
-aList = range(1, 21)
-
-print(change_list(aList, is_it_odd))
-
 
 # ---------- FUNCTION ANNOTATIONS ----------
 # It is possible to define the data types of attributes
